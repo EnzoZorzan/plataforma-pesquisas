7dfa7b4b40b83927a8d321537ed62a9a02171897
Enzo Lima <enzozorzanlima@gmial.com>
2025-12-19 14:49:30 -0300

    fwedw

--- src/main/java/com/plataforma/plataforma_pesquisas/PlataformaPesquisasApplication.java
+++ src/main/java/com/plataforma/plataforma_pesquisas/PlataformaPesquisasApplication.java
@@ -2,8 +2,12 @@ package com.plataforma.plataforma_pesquisas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.plataforma.plataforma_pesquisas")
+@EnableJpaRepositories("com.plataforma.plataforma_pesquisas")
 public class PlataformaPesquisasApplication {
 
 	public static void main(String[] args) {
